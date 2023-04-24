5a988ee60beca08caef5d8fe2583edbf9c7c2829
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-24 20:11:50 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,20 @@ exec { 'apt-update':                    # exec resource named 'apt-update'
   command => '/usr/bin/apt-get update'  # command this resource will run
 }
 
+# install Python3
+package { 'flask':
+  ensure  => 'installed',
+  require => Exec['apt install python3.8.10'],
+}
+
 # install flask from pip3
 package { 'flask':
   ensure  => 'installed',
   require => Exec['pip3 install flask==2.1.0'],
+}
+
+# install flask from pip3
+package { 'Werkzeug':
+  ensure  => 'installed',
+  require => Exec['pip3 install Werkzeug==2.1.1'],
 }
