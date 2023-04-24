cb946e47c4611d31ee7fc2f11820ab8949d0b94d
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-24 19:51:18 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,8 @@
+# execute 'apt-get update'
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update'  # command this resource will run
+}
+
 # install flask from pip3
 package { 'flask':
   ensure  => 'installed',
